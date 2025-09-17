5477dbda6800df061b21e38ed768034d3141a976
Adithyan P S <161561887+adithyanvlmna@users.noreply.github.com>
2025-09-17 09:52:25 +0530

    feat: setup app architecture and initial project structure

---
+++ lib/core/app_theme/app_textstyles.dart
@@ -0,0 +1,22 @@
+
+import 'package:ayurseva/core/app_theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+
+class AppTextStyles {
+static  TextStyle primaryText = TextStyle(
+  fontSize: 26,
+  fontWeight: FontWeight.bold,
+  color: AppColors.blackColor,
+);
+static  TextStyle secondaryGreyText = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+  color: AppColors.greyColor,
+);
+static  TextStyle secondaryBlackText = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.bold,
+  color: AppColors.blackColor,
+);
+}
